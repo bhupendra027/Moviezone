cf5984773e64d3c3ac566c869c1c3d4191cfedc5
Bhupendra Mahato <bhupendramht@outlook.com>
2025-05-12 11:01:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react'
+import Movies from './Movies'
+import {Movies} from    "./Data.js"
+
+
+const App = () => {
+  return (
+    <div>
+
+      <Movies/>
+
+      <Movies />
+    </div>
+  )
+}
+
+export default App
